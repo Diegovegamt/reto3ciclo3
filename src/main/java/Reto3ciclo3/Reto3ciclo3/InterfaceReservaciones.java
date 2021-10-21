16eb0802ce1c32765610aad868f3eba5f9898dc6
Diegovegamt <diego.vega.mt@correo.usa.edu.co>
2021-10-20 22:43:10 -0500

    first commit

---
+++ src/main/java/Reto3ciclo3/Reto3ciclo3/InterfaceReservaciones.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Reto3ciclo3.Reto3ciclo3;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author DiegoMV
+ */
+public interface InterfaceReservaciones extends CrudRepository<Reservaciones,Integer>{
+    
+}
---
+++ src/main/java/Reto3ciclo3/Reto3ciclo3/Reto3Application.java
@@ -0,0 +1,18 @@
+
+package Reto3ciclo3.Reto3ciclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author DiegoMV
+ */
+@SpringBootApplication
+public class Reto3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto3Application.class, args);
+	}
+
+}
